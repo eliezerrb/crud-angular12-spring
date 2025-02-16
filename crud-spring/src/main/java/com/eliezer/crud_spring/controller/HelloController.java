e4b6ac37acd24343060a9c7be5fd893404cfe395
eliezerrb <eliezerrezende@outlook.com>
2025-02-16 12:48:51 -0300

    Criação do projeto spring = hello world

---
+++ crud-spring/src/main/java/com/eliezer/crud_spring/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.eliezer.crud_spring.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping("/api/hello")
+public class HelloController {
+    
+    @GetMapping
+    public String hello() {
+        return "Hello!";
+    }
+}
