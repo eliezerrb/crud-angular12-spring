b2314bc1aa0933770e03403d6295ad1755830353
eliezerrb <eliezerrezende@outlook.com>
2025-02-21 22:54:47 -0300

    Adiciona seleção de categoria, tratamento de erro no formulário de cursos, chamando do backend HTTP POST para salvar os dados do formúlario

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -1,30 +1,32 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Course } from '../model/course';
 
-import { delay, first, tap } from 'rxjs/operators'
+import { first, tap } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CoursesService {
-
   // readonly previnir que eu faça modificações nesse valor
   // caminho do endpoint
-  private readonly API = 'api/courses'
+  private readonly API = 'api/courses';
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
   list() {
     // pipe pode manipular o retorno do get de maneira reativaa antes do retorno final
-    return this.httpClient.get<Course[]>(this.API)
-    .pipe(
+    return this.httpClient.get<Course[]>(this.API).pipe(
       // pegar a primeira resposta e fechar a conexão, isso porque não é um websocket
       first(),
       //delay(5000),
       // quando eu receber a lista de cursos eu faço alguma coisa, nessa caso mostra o log
-      tap(courses => console.log(courses))
+      tap((courses) => console.log(courses))
     );
-}
+  }
 
+  save(record: Course) {
+    // o pipe é para manipular o retorno do post
+   return this.httpClient.post<Course>(this.API, record).pipe(first());
+  }
 }
