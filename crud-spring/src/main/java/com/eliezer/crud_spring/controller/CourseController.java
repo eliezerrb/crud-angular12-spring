fff1787061d5a8884edeb5ac7b992e5cd941f38c
eliezerrb <eliezerrezende@outlook.com>
2025-02-23 16:53:35 -0300

    Formulário: API Spring: Criar Curso (HTTP POST) e implementa navegação ao cancelar, voltar usando Locale

--- crud-spring/src/main/java/com/eliezer/crud_spring/controller/CourseController.java
+++ crud-spring/src/main/java/com/eliezer/crud_spring/controller/CourseController.java
@@ -2,8 +2,13 @@ package com.eliezer.crud_spring.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.eliezer.crud_spring.model.Course;
@@ -26,5 +31,15 @@ public class CourseController {
     @GetMapping
     public List<Course> list() {
         return courseRepository.findAll();
+    }
+
+    // @RequestMapping(method = RequestMethod.POST)   
+    // @RequestBody consegue pegar o que está vindo no payload na chamada do post desde que as variaceis tenham o mesmo nome
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        return courseRepository.save(course);
+        // Se  estivesse retornando Course e não quisesse alterar o status -> return courseRepository.save(course);
+        // se não tivesse a anotação @ResponseStatus -> return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
     }
 }
