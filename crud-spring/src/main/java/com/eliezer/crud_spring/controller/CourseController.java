549436c26e6f0d5014c8609d25f6cad9d71ec858
eliezerrb <eliezerrezende@outlook.com>
2025-02-27 23:06:08 -0300

    Atualizar Curso: API Spring HTTP PUT

--- crud-spring/src/main/java/com/eliezer/crud_spring/controller/CourseController.java
+++ crud-spring/src/main/java/com/eliezer/crud_spring/controller/CourseController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -17,8 +18,6 @@ import com.eliezer.crud_spring.repository.CourseRepository;
 
 import lombok.AllArgsConstructor;
 
-
-
 @RestController
 @RequestMapping("/api/courses")
 // Anotação para gerar todoss os construtores pelo lombok
@@ -27,32 +26,48 @@ public class CourseController {
 
     // final para não alterar a instancia é uma boa prática
     private final CourseRepository courseRepository;
-    
+
     // @RequestMapping(method = RequestMethod.GET) é mesma coisa que @GetMapping
-    // @RequestMapping(method = RequestMethod.GET)    
+    // @RequestMapping(method = RequestMethod.GET)
     @GetMapping
     public List<Course> list() {
         return courseRepository.findAll();
     }
 
     // com @PathVariable é possível pegar o valor que está vindo na url
-    // ResponseEntity classe que permite retornar caso agente queira controlar o que esta indo de resposta na requisição
-    // .map(course -> ResponseEntity.ok(course)) -> se o (retorno) existir retorna ela
+    // ResponseEntity classe que permite retornar caso agente queira controlar o que
+    // esta indo de resposta na requisição
+    // .map(course -> ResponseEntity.ok(course)) -> se o (retorno) existir retorna
+    // ela
     @GetMapping("/{id}")
     public ResponseEntity<Course> findById(@PathVariable Long id) {
         return courseRepository.findById(id)
-                .map(course -> ResponseEntity.ok(course))
+                .map(recordFound -> ResponseEntity.ok(recordFound))
                 .orElse(ResponseEntity.notFound().build());
     }
-    
 
-    // @RequestMapping(method = RequestMethod.POST)   
-    // @RequestBody consegue pegar o que está vindo no payload na chamada do post desde que as variaceis tenham o mesmo nome
+    // @RequestMapping(method = RequestMethod.POST)
+    // @RequestBody consegue pegar o que está vindo no payload na chamada do post
+    // desde que as variaceis tenham o mesmo nome
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
     public Course create(@RequestBody Course course) {
         return courseRepository.save(course);
-        // Se  estivesse retornando Course e não quisesse alterar o status -> return courseRepository.save(course);
-        // se não tivesse a anotação @ResponseStatus -> return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
+        // Se estivesse retornando Course e não quisesse alterar o status -> return
+        // courseRepository.save(course);
+        // se não tivesse a anotação @ResponseStatus -> return
+        // ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Course> update(@PathVariable Long id, @RequestBody Course course) {
+        return courseRepository.findById(id)
+                .map(recordFound -> {
+                    recordFound.setName(course.getName());
+                    recordFound.setCategory(course.getCategory());
+                    Course updated = courseRepository.save(recordFound);
+                    return ResponseEntity.ok().body(updated);
+                })
+                .orElse(ResponseEntity.notFound().build());
     }
 }
