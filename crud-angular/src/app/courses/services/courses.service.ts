f6cc3b1c57d5c08412273ec7db59f55ad1ad50b8
eliezerrb <eliezerrezende@outlook.com>
2025-02-27 22:09:03 -0300

    Atualizar Curso: Salvando o Formulário

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -30,7 +30,20 @@ export class CoursesService {
   }
 
   save(record: Partial<Course>) {
+    if (record._id) {
+      return this.update(record);
+    }
+    return this.create(record);
+  }
+
+  private create(record: Partial<Course>) {
     // o pipe é para manipular o retorno do post
-   return this.httpClient.post<Course>(this.API, record).pipe(first());
+    return this.httpClient.post<Course>(this.API, record).pipe(first());
+  }
+
+  private update(record: Partial<Course>) {
+    return this.httpClient
+      .put<Course>(`${this.API}/${record._id}`, record)
+      .pipe(first());
   }
 }
