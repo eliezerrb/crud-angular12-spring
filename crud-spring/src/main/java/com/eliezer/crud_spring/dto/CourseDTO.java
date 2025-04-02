05bfe97d2da350787ddbaa439e17ab53395dfe6c
eliezerrb <eliezerrezende@outlook.com>
2025-04-02 11:05:47 -0300

    BACK - Adiciona validação personalizada para categorias no CourseDTO, criação de anotação - "Validando Enums com Java Bean Validations"

--- crud-spring/src/main/java/com/eliezer/crud_spring/dto/CourseDTO.java
+++ crud-spring/src/main/java/com/eliezer/crud_spring/dto/CourseDTO.java
@@ -1,20 +1,21 @@
 package com.eliezer.crud_spring.dto;
 
 import java.util.List;
+import java.util.Locale.Category;
 
+import com.eliezer.enums.validation.ValueOfEnum;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record CourseDTO(
      @JsonProperty("_id") Long id,
      @NotBlank @NotNull @Size(min = 3, max = 100) String name,
-     @NotNull @Size(max = 10) @Pattern(regexp = "Back-end|Front-end") String category,
+     @NotNull @Size(max = 10) @ValueOfEnum(enumClass = Category.class) String category,
      @NotNull @NotEmpty @Valid List<LessonDTO> lessons
 ) {
      
